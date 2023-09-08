1fc4d8502a8cd230f141cbc7dda357c8cdb8ea6b
Brandon Zenbook <tangbrandonk@gmail.com>
2023-09-07 22:19:13 -0700

    bot will play random moves

--- botAlgorithm.h
+++ botAlgorithm.h
@@ -1,39 +1,23 @@
 #ifndef BOTALGORITHM_H
 #define BOTALGORITHM_H
 #include "board.h"
+#include <random>
 
 //int analyzePosition(PlayingBoard board, int player){
 //    for(int i)
 //}
 void chooseMove(PlayingBoard &board, int player){
-    std::vector<std::vector<piece>> boardState = board.getBoard();
-    std::pair<int, int> reset;
-    int bestMove = 0;
-    int bestScore = 0;
-    for(int i = 0; i<7; i++){
-        for(int n = 0; n<6; n++){
-            if(boardState[n][i].color == 0){
-                reset = board.placePiece(i, player);
-                if(reset.first == -1 && reset.second == -1){
-                    continue;
-                }
-                int score = 0;
-                if(checkWin(boardState, player)){
-                    score = 100;
-                }
-                else{
-                    score = 0;
-                }
-                if(score > bestScore){
-                    bestScore = score;
-                    bestMove = i;
-                }
-                board.removePiece(reset.first, reset.second);
-                break;
-            }
-        }
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distribution(1, 7);
+    int col = distribution(gen);
+
+    if(board.placePiece(col, player).first == -1){
+        chooseMove(board, player);
     }
-    board.placePiece(bestMove, player);
+    else{
+        std::cout<<"Player "<<player<<" placed a piece in column "<<col<<std::endl;
+    }
 }
 
 #endif
